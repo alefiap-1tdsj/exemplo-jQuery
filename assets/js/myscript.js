fdaf3efa32039373fd4569760720309f93dab44e
logonrm <logonrm@fiap.com.br>
2017-05-12 12:04:44 -0300

    Exemplos JS

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,32 @@
-var n = navigator.userAgent;
+//var n = navigator.userAgent;
 
-document.write("<p>Informações do navegador</p>\n" + n);
+//document.write("<p>Informações do navegador</p>\n" + n);
+/*
+if (2<1) {
+  alert("Dois é menor que um!");
+}else {
+  alert("Dois não é menor que um!");
+}
+*/
+
+function validaCampo(nomeElemento) {
+
+  if (nomeElemento.tNuser.value == "") {
+    nomeElemento.tNuser.style="box-shadow:1px 1px 10px #ff0000;";
+    document.getElementById("iWarning1").innerHTML="*";
+    return false;
+  }else if(nomeElemento.tSenha.value == ""){
+    nomeElemento.tSenha.style="box-shadow:1px 1px 10px #ff0000;";
+    document.getElementById("iWarning2").innerHTML="*";
+    return false;
+  }else{
+    return true;
+  }
+
+}
+function validaCor(nomeInput) {
+
+    nomeInput.style="box-shadow:none;";
+    document.getElementById("iWarning1").innerHTML="";
+    document.getElementById("iWarning2").innerHTML="";
+}
